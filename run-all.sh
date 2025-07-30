c913dacb3a5c5cfecffc7cb2b48b138901c3d325
kevin <kevintxd73@gmail.com>
2025-07-30 13:44:05 -0300

    Adjusting db in docker and initialization script

--- run-all.sh
+++ run-all.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Exit immediately if any command fails
 set -e
 
 echo "🟡 Starting Besu local blockchain network..."
-
 cd besu
 ./startDev.sh
 cd ..
 
 echo "🟡 Starting PostgreSQL database with Docker Compose..."
+cd app/docker
+docker-compose -f docker-compose.db.yaml up -d
+cd ../..
 
-cd app/
-docker-compose up -d
+echo "⏳ Waiting for Postgres (docker exec)..."
+until docker exec goledger-db pg_isready -U admin -d goledger >/dev/null 2>&1; do
+  echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+echo "🟢 Postgres is up!"
 
 echo "🟢 Running Go application..."
+cd app
+go run main.go
